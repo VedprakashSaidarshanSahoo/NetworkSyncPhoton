c10a6918853c79430e6c9061718c41ca1ac349a9
Bedaprakash Saidarshan Sahoo <145172881+VedprakashSaidarshanSahoo@users.noreply.github.com>
2024-06-08 00:41:43 +0530

    Added Photon Engine. Default Movement and Shooting

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,95 @@
+using Photon.Pun;
+using System;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] NetworkManager m_NetworkManager;
+    [SerializeField] GameObject m_Player_Prefab;
+    [SerializeField] Transform[] m_Player_Instantiate_LOC;
+
+    public event Action GameOver;
+
+    private void OnEnable()
+    {
+        Init();
+        Subscribe();
+    }
+
+    private void OnDisable()
+    {
+        UnSubscribe();
+    }
+
+    private void Init()
+    {
+        NetworkCallbacks.ConnectServer();
+        m_NetworkManager = FindObjectOfType<NetworkManager>();
+    }
+
+    private void Subscribe()
+    {
+        m_NetworkManager.ConnectServer += JoinLobby;
+        m_NetworkManager.JoinLobby += JoinOrCreateRoom;
+        m_NetworkManager.JoinRoom += InstantiatePlayer;
+        m_NetworkManager.PlayerEntered += DisableCameras;
+    }
+
+    private void UnSubscribe()
+    {
+        m_NetworkManager.ConnectServer -= JoinLobby;
+        m_NetworkManager.JoinLobby -= JoinOrCreateRoom;
+        m_NetworkManager.JoinRoom -= InstantiatePlayer;
+        m_NetworkManager.PlayerEntered -= DisableCameras;
+    }
+
+    private void JoinLobby()
+    {
+        NetworkCallbacks.JoinLobby();
+    }
+
+    private void JoinOrCreateRoom()
+    {
+        NetworkCallbacks.JoinOrCreateRoom("My_Room", 2);
+    }
+
+    private void InstantiatePlayer()
+    {
+        NetworkCallbacks.DebugLogRich("INSTANTIATED PLAYER", 
+            "blue", 
+            NetworkCallbacks.DebugFont(FontStyle.bold), 
+            NetworkCallbacks.DebugFont(FontStyle.italic));
+
+        PhotonNetwork.Instantiate(m_Player_Prefab.name, m_Player_Instantiate_LOC[PhotonNetwork.LocalPlayer.ActorNumber - 1].position, Quaternion.identity);
+        PhotonNetwork.NickName = "Darshan";
+    }
+
+    private void DisableCameras(Photon.Realtime.Player _Player)
+    {
+        foreach (GameObject _pl in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            if (!_pl.GetComponent<PhotonView>().IsMine)
+            {
+                _pl.transform.GetChild(0).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    internal Transform GetCurrentPlayer()
+    {
+        foreach (GameObject _pl in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            if (_pl.GetComponent<PhotonView>().IsMine)
+            {
+                Debug.Log(_pl.name);
+                return _pl.transform;
+            }
+        }
+        return null;
+    }
+
+    internal void GameOverActivity()
+    {
+        GameOver?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,134 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Player : MonoBehaviourPunCallbacks
+{
+    [SerializeField] Rigidbody m_CharacterController;
+    [SerializeField] float m_Speed;
+    [SerializeField] int m_Score = 0;
+    [SerializeField] int m_HighScore = 0;
+    [SerializeField] GameObject m_WeaponPrefab;
+    [SerializeField] Transform m_WeaponSpawnPoint;
+    [SerializeField] float m_WeaponSpawnDelay = 5f;
+    [SerializeField] TMP_Text m_PlayerName;
+
+    private PhotonView photonView;
+    private Camera m_PlayerCamera;
+
+    private bool m_CanFire;
+    private bool m_GameOver;
+
+    private void Awake()
+    {
+        m_CanFire = true;
+        m_GameOver = false;
+        photonView = GetComponent<PhotonView>();
+        m_PlayerCamera = transform.GetChild(0).GetComponent<Camera>();
+
+        if (!photonView.IsMine)
+        {
+            m_PlayerCamera.gameObject.SetActive(false);
+        }
+    }
+
+    private void Start()
+    {
+        // Disable cameras of other players
+        DisableCameras();
+        photonView.RPC("PlayerName", RpcTarget.All);
+    }
+
+    private void Update()
+    {
+        if (photonView.IsMine)
+        {
+            Movement();
+            Action();
+
+            if (transform.position.y < -2 && !m_GameOver)
+            {
+                m_GameOver = true;
+            }
+        }
+    }
+
+    private void Action()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            NetworkCallbacks.DebugLogRich("Fire...", "red", NetworkCallbacks.DebugFont(FontStyle.italic), NetworkCallbacks.DebugFont(FontStyle.bold));
+            if (m_CanFire)
+            {
+                Shoot();
+            }
+        }
+    }
+
+    private void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward * z;
+        m_CharacterController.MovePosition(m_CharacterController.position + move * m_Speed * Time.deltaTime);
+    }
+
+    private void Shoot()
+    {
+        var _weapon = PhotonNetwork.Instantiate(m_WeaponPrefab.name, m_WeaponSpawnPoint.position, m_WeaponSpawnPoint.rotation);
+        _weapon.GetComponent<Rigidbody>().velocity = Vector3.forward * 50f;
+        Destroy(_weapon, 4f);
+        StartCoroutine(CanFire());
+    }
+
+    private IEnumerator CanFire()
+    {
+        m_CanFire = false;
+        yield return new WaitForSeconds(1f);
+        m_CanFire = true;
+    }
+
+    private void DisableCameras()
+    {
+        foreach (GameObject _pl in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            if (!_pl.GetComponent<PhotonView>().IsMine)
+            {
+                _pl.transform.GetChild(0).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    [PunRPC]
+    private void PlayerName()
+    {
+        m_PlayerName.text = PhotonNetwork.NickName;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        m_Score -= (int)damage;
+        NetworkCallbacks.DebugLogRich($"Player's Score: {m_Score}", "red", NetworkCallbacks.DebugFont(FontStyle.bold), NetworkCallbacks.DebugFont(FontStyle.italic));
+    }
+
+    private void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+            stream.SendNext(m_Score);
+            stream.SendNext(m_HighScore);
+        }
+        else
+        {
+            transform.position = (Vector3)stream.ReceiveNext();
+            transform.rotation = (Quaternion)stream.ReceiveNext();
+            m_Score = (int)stream.ReceiveNext();
+            m_HighScore = (int)stream.ReceiveNext();
+        }
+    }
+}
